722910f4dc98cb9682d0b6283415a7201eb6e5d8
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2022-09-02 20:28:04 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import { Provider } from 'react-redux';
 import './index.css';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>      
-        <App />
+    <Provider store={store}>    
+      <PersistGate loading={null} persistor={persistor}>
+         <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+          </BrowserRouter>
+        </PersistGate>  
         </Provider>
   </React.StrictMode>
 );
